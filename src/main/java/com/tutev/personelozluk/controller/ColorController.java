fd5652ea846c9f4a937cb25b1781dd2eb8108fba
ilerijava <bilalsezgin1985@gmail.com>
2015-06-13 18:50:56 +0300

    package declarations are updated

--- src/main/java/com/tutev/personelozluk/controller/ColorController.java
+++ src/main/java/com/tutev/personelozluk/controller/ColorController.java
@@ -1,3 +1,5 @@
+package com.tutev.personelozluk.controller;
+
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.List;
--- src/main/java/com/tutev/personelozluk/controller/LoginController.java
+++ src/main/java/com/tutev/personelozluk/controller/LoginController.java
@@ -1,3 +1,5 @@
+package com.tutev.personelozluk.controller;
+
 import java.io.IOException;
 
 import javax.servlet.ServletException;
