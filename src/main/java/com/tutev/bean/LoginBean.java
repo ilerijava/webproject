8d64e56f7535a849f8e924a05d74349b94f7f6b6
ilerijava <bilalsezgin1985@gmail.com>
2015-08-05 01:20:19 +0300

    giriş ekranı düzenlendi.

--- src/main/java/com/tutev/bean/LoginBean.java
+++ src/main/java/com/tutev/bean/LoginBean.java
@@ -1,6 +1,7 @@
 package com.tutev.bean;
 
 import com.tutev.util.MessageUtil;
+
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
@@ -10,64 +11,72 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ManagedProperty;
 import javax.faces.bean.RequestScoped;
+
 import java.io.Serializable;
 
 @ManagedBean(name = "loginView")
 @RequestScoped
 public class LoginBean implements Serializable {
-  /**
+	/**
    *
    */
-  private static final long serialVersionUID = 6196800544465885933L;
+	private static final long serialVersionUID = 6196800544465885933L;
 
-  private String userName = null;
-  private String password = null;
+	private String userName = null;
+	private String password = null;
 
-  @ManagedProperty(value = "#{authenticationManager}")
-  private AuthenticationManager authenticationManager = null;
+	@ManagedProperty(value = "#{authenticationManager}")
+	private AuthenticationManager authenticationManager = null;
 
-  public String login() {
-    try {
+	public String login() {
+		try {
+			Authentication auth = SecurityContextHolder.getContext()
+					.getAuthentication();
 
-      Authentication request = new UsernamePasswordAuthenticationToken(
-          this.getUserName(), this.getPassword());
-      Authentication result = authenticationManager.authenticate(request);
-      SecurityContextHolder.getContext().setAuthentication(result);
-    } catch (AuthenticationException e) {
-      MessageUtil.addInfoMessage(e.getMessage());
-      return "incorrect";
-    }
-    return "correct";
-  }
+			if (auth.getPrincipal() instanceof String) {
+				Authentication request = new UsernamePasswordAuthenticationToken(
+						this.getUserName(), this.getPassword());
 
-  public String logout() {
-    SecurityContextHolder.clearContext();
-    return "loggedout";
-  }
+				Authentication result = authenticationManager
+						.authenticate(request);
 
-  public AuthenticationManager getAuthenticationManager() {
-    return authenticationManager;
-  }
+				SecurityContextHolder.getContext().setAuthentication(result);
+			}
+		} catch (AuthenticationException e) {
+			MessageUtil.addInfoMessage(e.getMessage());
+			return "incorrect";
+		}
+		return "correct";
+	}
 
-  public void setAuthenticationManager(
-      AuthenticationManager authenticationManager) {
-    this.authenticationManager = authenticationManager;
-  }
+	public String logout() {
+		SecurityContextHolder.clearContext();
+		return "loggedout";
+	}
 
-  public String getUserName() {
-    return userName;
-  }
+	public AuthenticationManager getAuthenticationManager() {
+		return authenticationManager;
+	}
 
-  public void setUserName(String userName) {
-    this.userName = userName;
-  }
+	public void setAuthenticationManager(
+			AuthenticationManager authenticationManager) {
+		this.authenticationManager = authenticationManager;
+	}
 
-  public String getPassword() {
-    return password;
-  }
+	public String getUserName() {
+		return userName;
+	}
 
-  public void setPassword(String password) {
-    this.password = password;
-  }
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
 
 }
